6180e7c9527c4b40ea800bfbcf0f0d200e07293a
benfonty <benfonty@gmail.com>
2021-12-29 22:58:42 +0100

    week 3 first task

--- Kotlin for Java Developers. Week 3/Nice String/Task/src/nicestring/NiceString.kt
+++ Kotlin for Java Developers. Week 3/Nice String/Task/src/nicestring/NiceString.kt
@@ -1,5 +1,13 @@
 package nicestring
 
 fun String.isNice(): Boolean {
-    TODO()
+    val doesNotContainsEvilSubstrings = if (this.contains(Regex("bu|ba|be"))) 0 else 1
+
+    val containsThreeVowels = if (this.filter { it in "aeiou" }.length >= 3)  1 else 0
+
+    val containsDoubleLetters = if (this.windowed(2).any {it[0] == it[1]}) 1 else 0
+
+    return containsDoubleLetters + doesNotContainsEvilSubstrings + containsThreeVowels >= 2
+
+
 }
