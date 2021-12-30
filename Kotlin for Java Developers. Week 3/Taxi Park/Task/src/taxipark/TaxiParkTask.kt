476ec1f0c82e87291c7329228fb1109f7eabe4d2
benfonty <benfonty@gmail.com>
2021-12-30 18:49:06 +0100

    midle of second exercicse of week 3

--- Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -3,20 +3,37 @@ package taxipark
 /*
  * Task #1. Find all the drivers who performed no trips.
  */
-fun TaxiPark.findFakeDrivers(): Set<Driver> =
-        TODO()
+fun TaxiPark.findFakeDrivers(): Set<Driver>  {
+    val nonFakeDrivers = this.trips.map {it.driver}
+    return this.allDrivers.minus(nonFakeDrivers)
+}
+
 
 /*
  * Task #2. Find all the clients who completed at least the given number of trips.
  */
 fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> =
-        TODO()
+    if (minTrips == 0)
+        this.allPassengers
+    else
+        this.trips
+                // TODO replace the two following methods by an alternative to groupby
+            .flatMap { trip -> trip.passengers.map {trip to it } }
+            .groupBy { it.second }
+            .filter { (_, trips)  -> trips.size >= minTrips}
+            .keys
+
 
 /*
  * Task #3. Find all the passengers, who were taken by a given driver more than once.
  */
 fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> =
-        TODO()
+    this.trips
+        .filter {it.driver == driver}
+        .flatMap { trip -> trip.passengers.map {trip.driver to it } }
+        .groupBy { it.second }
+        .filter { (_, trips)  -> trips.size > 1}
+        .keys
 
 /*
  * Task #4. Find the passengers who had a discount for majority of their trips.
@@ -29,7 +46,13 @@ fun TaxiPark.findSmartPassengers(): Set<Passenger> =
  * Return any period if many are the most frequent, return `null` if there're no trips.
  */
 fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
-    return TODO()
+    return trips
+        .groupBy { trip ->
+            val startOfRange = trip.duration / 10 * 10
+            startOfRange..(startOfRange + 9)
+         }
+        .maxBy { it.value.size }
+        ?.key
 }
 
 /*
@@ -37,5 +60,9 @@ fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
  * Check whether 20% of the drivers contribute 80% of the income.
  */
 fun TaxiPark.checkParetoPrinciple(): Boolean {
-    TODO()
+    val trips80PercentOfTheIncome: Set<Trip> = TODO()
+    val drivers80PercentOfIncome = trips80PercentOfTheIncome
+        .map { it.driver}
+        .toSet()
+    return drivers80PercentOfIncome.size / allDrivers.size * 100 == 20
 }
