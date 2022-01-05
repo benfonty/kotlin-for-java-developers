aabf7a2b8211c07a7ff9d76671a25a1a4d0283e5
benfonty <benfonty@gmail.com>
2022-01-05 22:48:31 +0100

    rationals

--- Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/Rational.kt
+++ Kotlin for Java Developers. Week 4/Rationals/Task/src/rationals/Rational.kt
@@ -1,6 +1,72 @@
 package rationals
 
+import java.lang.IllegalArgumentException
+import java.math.BigInteger
 
+private val zero: BigInteger = BigInteger.valueOf(0)
+private val one: BigInteger = BigInteger.valueOf(1)
+
+class Rational(val numerator: BigInteger, val denominator: BigInteger): Comparable<Rational> {
+
+    init { if (denominator == zero) throw IllegalArgumentException() }
+
+    operator fun plus(other: Rational): Rational = when {
+        this.numerator == zero -> other
+        other.numerator == zero -> this
+        else -> this.numerator * other.denominator + other.numerator * this.denominator divBy this.denominator * other.denominator
+    }
+    operator fun minus(other: Rational): Rational = this + -other
+    operator fun times(other: Rational): Rational = this.numerator * other.numerator divBy this.denominator * other.denominator
+    operator fun div(other: Rational): Rational = when (other.numerator) {
+        zero -> zero divBy this.denominator
+        else -> this * (other.denominator divBy other.numerator)
+    }
+    operator fun rangeTo(other: Rational): ClosedRange<Rational> = RangeRational(this, other)
+    override operator fun compareTo(other: Rational): Int {
+        val _this = minusToNumerator()
+        val _other = other.minusToNumerator()
+        return (_this.numerator * _other.denominator).compareTo(_other.numerator * _this.denominator)
+    }
+    override fun toString(): String {
+        val tmp = minusToNumerator()
+        val gcd = tmp.numerator.gcd(tmp.denominator)
+        var newNumerator = tmp.numerator / gcd
+        var newDenominator = tmp.denominator / gcd
+        if (newDenominator == one) return "$newNumerator"
+        return "$newNumerator/$newDenominator"
+    }
+    operator fun unaryMinus(): Rational = -this.numerator divBy this.denominator
+    override fun equals(other:Any?): Boolean = when (other) {
+        is Rational -> this.numerator * other.denominator == this.denominator * other.numerator
+        else -> false
+    }
+
+    private fun minusToNumerator(): Rational =
+        if (this.denominator > zero) {
+            this
+        } else {
+            - this.numerator divBy - this.denominator
+        }
+
+}
+
+class RangeRational(override val start: Rational, override val endInclusive: Rational) : ClosedRange<Rational>
+
+
+infix fun BigInteger.divBy(other: BigInteger): Rational = Rational(this, other)
+
+infix fun Long.divBy(other: Long): Rational = BigInteger.valueOf(this) divBy BigInteger.valueOf(other)
+
+infix fun Int.divBy(other: Int): Rational = this.toBigInteger() divBy other.toBigInteger()
+
+fun String.toRational(): Rational {
+    val splitList = this.split("/")
+    return when (splitList.size) {
+        1 -> toBigInteger() divBy one
+        2 -> splitList[0].toBigInteger() divBy splitList[1].toBigInteger()
+        else -> throw IllegalArgumentException()
+    }
+}
 
 fun main() {
     val half = 1 divBy 2
